10428a6c83ac35fee7a8a8a67d3c4478806cee82
Ayush Poudel <ayushpoudel@usf.edu>
2025-05-19 18:18:24 -0400

    Terraform does not support Dynatrace V2 dashboards

--- examples/application-availibility-stg/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "dynatrace_api_url" {
-  type        = string
-  description = "API URL of dynatrace server"
-}
-variable "dynatrace_api_token" {
-  type        = string
-  description = "API token to programatically connect to the server"
-}
